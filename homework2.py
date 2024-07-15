8f938b2f2dd763ad8924e220b281c10af62d4d8d
d1r0Il <ip193578@gmail.com>
2024-07-15 20:49:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course = "Pytom"
+task = 12
+time_of_spend  = 1.5
+x = time_of_spend / task
+print (course, 'всего задач', task, 'затрачено часов', time_of_spend,'среднее время выполнения', x, 'часа' )
