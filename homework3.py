bec7f202523dc043c966d2f543a26472627ef805
d1r0Il <ip193578@gmail.com>
2024-07-17 20:45:06 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = "Igor"
+print ("Name", name, type(name))
+age = 18
+print ("Age", age, type (age))
+age = age + 2
+print ("New_age", age, type(age))
+is_student = True
+print ("Is_Student", is_student, type(is_student))
