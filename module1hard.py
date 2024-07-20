86968acaf0bc8360cd88303812f6e41065d11a2b
d1r0Il <ip193578@gmail.com>
2024-07-20 20:41:36 +0300

    module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -1,7 +1,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-list = sorted( list(students ) )
-list1 = [ (sum (grades[0]) / len (grades[0])),(sum (grades[1]) / len (grades[1])),
+lst = sorted( list(students ) )
+lst1 = [ (sum (grades[0]) / len (grades[0])),(sum (grades[1]) / len (grades[1])),
 (sum (grades[2]) / len ( grades[2])),(sum (grades[3]) / len (grades[3])),(sum (grades[4]) / len (grades[4]))]
-averagе_value = (dict (zip (list,list1)))
+averagе_value = (dict (zip (lst,lst1)))
 print (averagе_value)
