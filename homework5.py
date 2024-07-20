9d512459c3bbd02d2c6526fc0d76309d3b32f41b
d1r0Il <ip193578@gmail.com>
2024-07-20 14:55:26 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, 3, "Safage"
+print (immutable_var)
+# immutable_var [-1] = "danger"
+# print(immutable_var)
+# Кортеж предназнаечен для храненя списка без изменения значений в нем
+mutable_list = ["pen", "notebook", "rubber", ]
+print (mutable_list)
+mutable_list [1] = "laptop"
+print (mutable_list)
