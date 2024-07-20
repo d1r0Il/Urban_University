af773d1232dc1e4abb353c728fe39b1219f319d3
d1r0Il <ip193578@gmail.com>
2024-07-20 15:47:24 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Igor' : 2004, 'Nikita' : 2005, 'Andrey' : 1996}
+print (my_dict)
+print (my_dict .get('Nikita'))
+print (my_dict .get('Anna'))
+my_dict .update({'Sasha': 2000, 'Artem': 2006})
+print (my_dict)
+my_dict .pop('Andrey')
+print (my_dict)
+my_set = {1,4,5, 3, 3, 5, 2, 2 ,1, 1,}
+print(my_set)
+my_set .add(6)
+my_set .add(8)
+print (my_set .discard(4))
+print (my_set)
